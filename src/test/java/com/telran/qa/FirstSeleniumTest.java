dcaf51d3ae9f50a02cf76084b4fb4fe2af6cc274
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-01-21 21:16:46 -0500

    Finished

---
+++ src/test/java/com/telran/qa/FirstSeleniumTest.java
@@ -0,0 +1,46 @@
+package com.telran.qa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Locale;
+
+public class FirstSeleniumTest {
+    WebDriver wd;
+
+    //before -  setUp(){}
+    @BeforeMethod
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("https://www.google.com/");
+
+    }
+
+    //Test
+    @Test
+    public void openGoogleTest(){
+        System.out.println("site opened!");
+
+        wd.findElement(By.name("q")).click();
+        wd.findElement(By.name("q")).sendKeys("Selenium" + Keys.ENTER);
+
+       String text = wd.findElement(By.tagName("h3")).getText();
+
+       Assert.assertEquals(text.toLowerCase(), "seleniumhq browser automation");
+
+    }
+
+    //after - tearDown(){}
+    @AfterMethod
+    public void tearDown(){
+        wd.quit();
+        //wd.close();  - closed only the New tab in a browser
+    }
+
+}
